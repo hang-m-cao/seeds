025b012e65ee92d119bd627ee9977a22779c3a18
Hang Cao <hmcao1@uci.edu>
2021-06-28 13:55:11 -0700

    bottom navigation + start page fragments

---
+++ app/src/main/java/com/example/tshlib/LearnFragment.kt
@@ -0,0 +1,14 @@
+package com.example.tshlib
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class LearnFragment: Fragment(R.layout.fragment_learn) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
