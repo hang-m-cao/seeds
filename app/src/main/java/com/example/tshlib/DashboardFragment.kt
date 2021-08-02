2134c5df5f9634736b11fffd275eb87ed2bf39b9
Shiying Lin <shiying6120@gmail.com>
2021-08-01 21:26:58 -0400

    home page completed

--- app/src/main/java/com/example/tshlib/DashboardFragment.kt
+++ app/src/main/java/com/example/tshlib/DashboardFragment.kt
@@ -1,14 +1,22 @@
 package com.example.tshlib
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 
 class DashboardFragment: Fragment(R.layout.fragment_dashboard) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.take_quiz_btn).setOnClickListener {
+            val intent = Intent(activity, QuizStartingPage::class.java)
+            activity?.startActivity(intent)
+        }
     }
+
+
 }
