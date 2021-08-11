def5a880fd42a2e1caf95b9a290e7f052185d175
Hang Cao <hmcao1@uci.edu>
2021-08-11 13:46:19 -0700

    add progress loading bar

--- app/src/main/java/com/example/tshlib/ArticleViewActivity.kt
+++ app/src/main/java/com/example/tshlib/ArticleViewActivity.kt
@@ -1,11 +1,14 @@
 package com.example.tshlib
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_article_view.*
 
 
@@ -14,15 +17,16 @@ class ArticleViewActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_article_view)
 
-        val articleWebView = findViewById<WebView>(R.id.article_webview)
-        articleWebView.webViewClient = WebViewClient()
+        val articleWebView: WebView = findViewById(R.id.article_webview)
+        val progressBar: ProgressBar = findViewById(R.id.progress_bar)
+        articleWebView.webViewClient = ArticleWebClient(progressBar)
+        articleWebView.settings.javaScriptEnabled = true
 
         done_text.setOnClickListener {
             done_text.setTextColor(resources.getColor(R.color.light_gray))
             Handler(Looper.getMainLooper()).postDelayed({
                 super.onBackPressed()
             }, 50)
-
         }
 
         if (intent.hasExtra("link")) {
@@ -40,5 +44,28 @@ class ArticleViewActivity : AppCompatActivity() {
         } else {
             super.onBackPressed()
         }
+    }
+}
+
+class ArticleWebClient(private val progressBar: ProgressBar) : WebViewClient() {
+    override fun onPageFinished(view: WebView, url: String) {
+        progressBar.visibility = View.GONE
+        super.onPageFinished(view, url)
+    }
+
+    override fun onPageStarted(view: WebView, url: String, favicon: Bitmap?) {
+        if (favicon != null) {
+            super.onPageStarted(view, url, favicon)
+        }
+
+    }
+
+    override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+        view.loadUrl(url)
+        return true
+    }
+
+    override fun onReceivedError(view: WebView, errorCode: Int, description: String, failingUrl: String) {
+        view.loadUrl("file:///android_asset/error.html")
     }
 }
