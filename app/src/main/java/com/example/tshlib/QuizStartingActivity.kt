f45cdf1f66193e28d534e587c76c07c2c41bfbdb
AnoushkaGupta <52936225+AnoushkaGupta@users.noreply.github.com>
2021-08-11 19:23:12 -0700

    failed attempt at quiz

--- app/src/main/java/com/example/tshlib/QuizStartingActivity.kt
+++ app/src/main/java/com/example/tshlib/QuizStartingActivity.kt
@@ -3,6 +3,7 @@ package com.example.tshlib
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class QuizStartingActivity : AppCompatActivity() {
@@ -10,6 +11,12 @@ class QuizStartingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_starting)
+
+        val startBtn : Button = findViewById(R.id.start_quiz_btn)
+         startBtn.setOnClickListener{
+             val intent = Intent(this, QuizActivity::class.java)
+             startActivity(intent)
+         }
 
 
         val bottomNav: BottomNavigationView = findViewById(R.id.bottom_navigation)
